d0c5af7560ecf24981f9e2f21ecad6cc459ad226
Daniel <danieloladele7@gmail.com>
2023-05-13 05:59:04 +0200

    Task 3 done, BaseModel class created

---
+++ models/base_model.py
@@ -0,0 +1,60 @@
+"""
+A module to implement BaseModel class
+"""
+
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    This class defines all common attributes/methods for other classes
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the BaseModel class
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ('created_at', 'updated_at'):
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+
+    def __str__(self):
+        """
+        Returns the string representation of BaseModel object.
+        [<class name>] (<self.id>) <self.__dict__>
+        """
+        clsname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clsname, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates 'self.updated_at' with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of __dict__
+        of the instance:
+
+        - using self.__dict__, only instance attributes set will be returned
+        - a key __class__ is added with the class name of the object
+        - created_at and updated_at must be converted to string object in ISO
+        object
+        """
+        clsdict = self.__dict__.copy()
+        clsdict["__class__"] = self.__class__.__name__
+        clsdict["created_at"] = self.created_at.isoformat()
+        clsdict["updated_at"] = self.updated_at.isoformat()
+        return clsdict
